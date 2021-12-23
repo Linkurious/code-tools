fd48c02ef581b1405b86f4461812e5c37d8b363b
Alexander Milevski <alexander@linkurio.us>
2021-12-23 17:14:52 +0100

    v0.0.6 (#6)
    * Bump patch version
    
    * v0.0.6 added npmrc cascading script
    
    * Added engines
    
    * Docs
    
    Co-authored-by: Linkurious ci bot <infra@linkurio.us>

--- bin/sync_versions.js
+++ bin/sync_versions.js
@@ -7,27 +7,27 @@
   - .bumpversion.cfg
 */
 
-const fs = require('fs');
-const path = require('path');
-const { version } = require(path.join(process.cwd(), 'package.json'));
-const { promisify } = require('util');
+import fs from "fs/promises";
+import path from "path";
+import chalk from "chalk";
 
-const readFile = promisify(fs.readFile);
-const writeFile = promisify(fs.writeFile);
+const cfgPath = path.join(process.cwd(), ".bumpversion.cfg");
+const versionPath = path.join(process.cwd(), ".version");
 
-const cfgPath = path.join(process.cwd(), '.bumpversion.cfg');
-const versionPath = path.join(process.cwd(), '.version');
+let version = "0.0.0";
 
-readFile(cfgPath, 'utf8')
-  .then(cfg => {
+fs.readFile(path.join(process.cwd(), "package.json"), "utf8")
+  .then((packageJson) => (version = JSON.parse(packageJson).version))
+  .then(() => fs.readFile(cfgPath, "utf8"))
+  .then((cfg) => {
     const updated = cfg.replace(
       /current_version\s*=\s*[^\n]*/i,
       `current_version = ${version}`
     );
-    return writeFile(cfgPath, updated, 'utf8');
+    return fs.writeFile(cfgPath, updated, "utf8");
   })
-  .then(() => writeFile(versionPath, version, 'utf8'))
-  .catch(e => {
-    console.error('sync_versions:', e.path, 'does not exist');
+  .then(() => fs.writeFile(versionPath, version, "utf8"))
+  .catch((e) => {
+    console.error(chalk.red("sync_versions:"), e.path, "does not exist");
     process.exit(1);
   });
